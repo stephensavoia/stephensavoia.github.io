03fc7d6f3206bb050fe666763b194fe3d803cb99
stephensavoia <ssavoia@gmail.com>
2024-07-25 00:31:52 -0400

    some hero banner work

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,18 +1,15 @@
+// Remove focus from menu (i.e. close menu, because that's how daisyui works),
+// when the menu button is clicked after it is already in focus (i.e. opened)
+
 const menuBtn = document.getElementById("menuBtn");
-let isMenuOpen = false;
+const menu = document.getElementById("menu");
 let menuShouldClose = false;
 
-menuBtn.addEventListener("focus", () => {
-  isMenuOpen = true;
-});
-
-menuBtn.addEventListener("blur", () => {
-  isMenuOpen = false;
-  menuShouldClose = false;
-});
-
-menuBtn.addEventListener("mousedown", (e) => {
-  if (isMenuOpen) {
+menuBtn.addEventListener("mousedown", () => {
+  if (
+    document.activeElement === menuBtn ||
+    menu.contains(document.activeElement)
+  ) {
     menuShouldClose = true;
   }
 });
@@ -23,7 +20,11 @@ menuBtn.addEventListener("click", () => {
   }
 });
 
-document.querySelectorAll(".menu a").forEach((link) => {
+menuBtn.addEventListener("blur", () => {
+  menuShouldClose = false;
+});
+
+document.querySelectorAll("#menu a").forEach((link) => {
   link.addEventListener("click", () => {
     document.activeElement.blur();
   });
